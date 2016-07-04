6e2ac46508dbb4f5a6fcc128e5e881dde8a6ab11
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-07-04 18:44:01 -0500

    Se comentan rutas en el controlador

--- controllers/index.js
+++ controllers/index.js
@@ -1,47 +1,47 @@
-// Controllers
+//  Controllers
 
 exports.home = function (req, res) {
   res.render('index');
 }
 
 exports.about = function(req, res) {
-  res.render('acercade');
-  // res.render('work_in_progress');
+  // res.render('acercade');
+   res.render('work_in_progress');
 }
 
 exports.portafolio = function(req, res) {
-  //res.render('portafolio');
+  // res.render('portafolio');
   res.render('work_in_progress');
 }
 
 exports.perfiles = function(req, res) {
   res.render('perfiles');
-  //res.render('work_in_progress');
+  // res.render('work_in_progress');
 }
 
 exports.perfil = function(req, res) {
   var name = req.params.name;
   res.render('profiles/' + name);
-  //res.render('work_in_progress');
+  // res.render('work_in_progress');
 }
 
 exports.apoyanos = function(req, res) {
-  //res.render('apoyanos');
+  // res.render('apoyanos');
   res.render('work_in_progress');
 }
 
 exports.comunidad = function(req, res) {
-  //res.render('comunidad');
+  // res.render('comunidad');
   res.render('work_in_progress');
 }
 
 exports.blog = function(req, res) {
-  //res.render('blog');
+  // res.render('blog');
   res.render('work_in_progress');
 }
 
 exports.eventos = function(req, res) {
-  //res.render('eventos');
+  // res.render('eventos');
   res.render('work_in_progress');
 }
 
