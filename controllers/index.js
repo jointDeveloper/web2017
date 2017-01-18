b4d55f576a0a5a2d3be8d460919b3027aa8f1573
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-01-18 08:54:50 -0500

    Reparado JSON de Eventos

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@ const event = require('../data/event.json');
 
 exports.index = function(req, res) {
   const profiles = profile.profiles[0];
-  // console.log(profiles[0]["john"].name);
-  // for (i in profiles[0]) {
-  //   console.log(i);
-  // }
   const next = event.next;
   const charla = event.charlas;
-  // console.log(events);
+  const {events} = event;
+  console.log(events);
 
-  res.render('index', {profiles: profiles, next: next, charlas: charla});
+  res.render('index', {
+                        profiles: profiles,
+                        next: next,
+                        charlas: charla,
+                        events: events
+                      });
 };
