e4175c4a1dbbeeb6bb2c051ab7b554da5a36219d
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-01-27 11:36:51 -0500

    Sponsors

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,20 @@
 const profile = require('../data/profile.json');
 const event = require('../data/event.json');
+const sponsor = require('../data/sponsor.json');
 
 exports.index = function(req, res) {
   const profiles = profile.profiles[0];
   const next = event.next;
   const charla = event.charlas;
   const {events} = event;
-  // console.log(events);
+  const sponsors = sponsor.sponsor[0];
+  // console.log(sponsors);
 
   res.render('index', {
     profiles: profiles,
     next: next,
     charlas: charla,
-    events: events
+    events: events,
+    sponsors: sponsors
   });
 };
