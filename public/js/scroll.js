9d4f3e9fc8323bb78e7aa1f4cb435c715b897e0b
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-20 16:56:29 -0500

    fix typo

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,14 +1,15 @@
-(function($) {
-  $(function() {
+($ => {
+  $(() => {
     $('a[href^="#"]').on('click', function(ev) {
       const target = $(this.getAttribute('href'));
       if (typeof target !== 'undefined') {
         ev.preventDefault();
-        $('html, body').stop().animate(
-          {
-            scrollTop: target.offset().top,
+        $('html, body')
+          .stop()
+          .animate({
+            scrollTop: target.offset().top
           },
-          1000,
+          1000
         );
       }
     });
