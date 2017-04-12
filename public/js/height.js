55dfdf241f92a3af5cbd96b9bf2c6f963661350d
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-04-11 22:46:48 -0500

    Sponsors evento RailsGirls Pereira

--- public/js/height.js
+++ public/js/height.js
@@ -1,8 +1,11 @@
 (function($) {
   $(function() {
     function changeHeightEvent() {
-      let event = $(".event");
-      let overlay = $(".overlay");
+      let event = $('.event');
+      let overlay = $('.overlay');
+      let person = $('.person');
+      let link = $('.link');
+      let sponsor = $('.sponsor');
       let maxHeight = 0;
 
       for (let i = 0; i < event.length; i++) {
@@ -15,11 +18,39 @@
         maxHeight = Math.max(maxHeight, height);
       }
 
-      for (let i = 0; i < event.length; i++)
-        $(event[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < person.length; i++) {
+        let height = Number($(person[i]).css('height').split('px')[0]);
+        maxHeight = Math.max(maxHeight, height);
+      }
 
-      for (let i = 0; i < overlay.length; i++)
-        $(overlay[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < link.length; i++) {
+        let height = Number($(link[i]).css('height').split('px')[0]);
+        maxHeight = Math.max(maxHeight, height);
+      }
+
+      for (
+        let i = 0;
+        i < event.length;
+        i++
+      ) $(event[i]).css('height', `${maxHeight}px`);
+
+      for (
+        let i = 0;
+        i < overlay.length;
+        i++
+      ) $(overlay[i]).css('height', `${maxHeight}px`);
+
+      for (
+        let i = 0;
+        i < person.length;
+        i++
+      ) $(person[i]).css('height', `${maxHeight}px`);
+
+      for (
+        let i = 0;
+        i < link.length;
+        i++
+      ) $(link[i]).css('height', `${maxHeight}px`);
     }
 
     changeHeightEvent();
