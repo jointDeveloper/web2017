9f386f30a8f16f1051671830f5a1bf21282d5286
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-04-11 22:54:48 -0500

    Algunos colaboradores agregados

--- public/js/height.js
+++ public/js/height.js
@@ -1,5 +1,5 @@
-(function($) {
-  $(function() {
+($ => {
+  $(() => {
     function changeHeightEvent() {
       let event = $('.event');
       let overlay = $('.overlay');
@@ -28,29 +28,17 @@
         maxHeight = Math.max(maxHeight, height);
       }
 
-      for (
-        let i = 0;
-        i < event.length;
-        i++
-      ) $(event[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < event.length; i++)
+        $(event[i]).css('height', `${maxHeight}px`);
 
-      for (
-        let i = 0;
-        i < overlay.length;
-        i++
-      ) $(overlay[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < overlay.length; i++)
+        $(overlay[i]).css('height', `${maxHeight}px`);
 
-      for (
-        let i = 0;
-        i < person.length;
-        i++
-      ) $(person[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < person.length; i++)
+        $(person[i]).css('height', `${maxHeight}px`);
 
-      for (
-        let i = 0;
-        i < link.length;
-        i++
-      ) $(link[i]).css('height', `${maxHeight}px`);
+      for (let i = 0; i < link.length; i++)
+        $(link[i]).css('height', `${maxHeight}px`);
     }
 
     changeHeightEvent();
